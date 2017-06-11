9aa1299f07b02797e87f20a2504e884e705c4790
objectiveabc <garysfw@outlook.com>
2017-06-10 20:53:37 -0700

    more basic functions added

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,22 @@
 def greeting(name):
     print 'Hello %s' %name
 
-name = raw_input("Enter your name -> ")
+def addnum(n1,n2):
+    return n1 + n2
 
+def ageCheck(age):
+    if age <= 12:
+        print "Damn you're a kid"
+    elif age > 12 and age < 21:
+        print "No booze for you"
+    else:
+        print "%s you're good to go. Let's get wasted!" %name
+    
+name = raw_input("Enter your name -> ")
 greeting(name)
+number1 = int(input("Think of a number -> "))
+number2 = int(input("Think of another number -> "))
+print "Tada, those two numbers add up to ->", addnum(number1,number2)
+print "Now we've got that out of the way, how old are you? -> "
+age = int(input())
+ageCheck(age)
